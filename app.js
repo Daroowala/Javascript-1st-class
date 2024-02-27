8a3023314a418a7cfc9b9284d058816406152b8d
my name <blueshaperd3@gmail.com>
2024-02-28 03:23:02 +0500

    ali class javascript start

---
+++ app.js
@@ -0,0 +1,47 @@
+alert("Hello World");
+//var a = 10;
+//var b = 5;
+//var c = a+b;
+//alert(c);
+
+
+//var a = 10;
+//var b = 5;
+//var c = a+b;
+//console.log(c);
+
+
+var x = 5;
+var y = "ali";
+var z = y+x;
+alert(z);
+
+
+var a = 20;
+var b = "10";
+var c = a*b;
+alert(c);
+
+
+var a = 3;
+var b = 5;
+var c = 7;
+var d = b-a*c;
+alert(d);
+
+
+var a = 3;
+var b = 5;
+var c = 7;
+var d = (b - a) * c;
+alert(d);
+
+
+var a = 5;
+var b = a++;
+alert (b);
+
+
+var a = 5;
+var b = ++a;
+alert (b);
